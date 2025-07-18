ab1feea51aa98b6e275f50fc484a5e55a30ce159
Max112358 <97316904+Max112358@users.noreply.github.com>
2025-07-17 19:01:32 -0700

    changing char limit to 4000

--- backend/config/posts.js
+++ backend/config/posts.js
@@ -2,7 +2,7 @@
 const postsConfig = {
   // Maximum number of characters allowed in post content
   // Set to null or 0 to character limit
-  characterLimit: 300,
+  characterLimit: 4000,
 };
 
 module.exports = postsConfig;
